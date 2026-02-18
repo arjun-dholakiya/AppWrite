20028e87bfd0d974809f31ee08bd0aa4561b2df5
Arjun Dholakiya <arjundholakiyav@gmail.com>
2026-02-18 18:15:20 +0530

    adding database services

--- src/appwrite/authService.js
+++ src/appwrite/authService.js
@@ -1,20 +1,60 @@
 import config from '../config/config';
 import { Client, Account, ID } from 'appwrite';
 
-class AuthService {
+class AuthServices {
   client = new Client();
   account;
 
   constructor() {
     this.client
-      .setEndpoint(config.appwriteUrl)
-      .setProject(config.appwriteProjectId);
+      .setProject(config.appwriteProjectId)
+      .setEndpoint(config.appwriteUrl);
     this.account = new Account(this.client);
   }
+  async createUser({ name, email, password }) {
+    try {
+      const user = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (user) {
+        // call another method
+        return this.login({ email, password });
+      } else {
+        return user;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 
-  
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
-const authService = new AuthService();
+const authServices = new AuthServices();
 
-export default AuthService;
+export default authServices;
