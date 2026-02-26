54f12415ff6d2e2759c901158a9a8c1303f085cc
Arjun Dholakiya <arjundholakiyav@gmail.com>
2026-02-26 10:47:44 +0530

    adding more features

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authServices from './appwrite/authService';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  
-  return <h1>Hello</h1>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authServices
+      .getUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO:{/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
