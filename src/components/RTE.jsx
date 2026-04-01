9f6f2b50d2ca9ccafad711158bb71e5357bdb469
Arjun Dholakiya <arjundholakiyav@gmail.com>
2026-04-01 22:48:19 +0530

    adding more features

---
+++ src/components/RTE.jsx
@@ -0,0 +1,9 @@
+import { Editor } from '@tinymce/tinymce-react';
+import React from 'react';
+import { Controller } from 'react-hook-form';
+
+export default function RTE() {
+  return (<div>
+
+  </div>);
+}
